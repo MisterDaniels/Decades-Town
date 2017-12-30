5c0276b09650fac1d43c91b64bb9a3d0851119a7
Daniel Silva <dodosilva51@gmail.com>
2017-12-30 02:26:25 -0200

    Game controller for variables, like money, buildings and population. Simple text UI to change the values realtime. Setup of the first scene!

---
+++ _CameraUI/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Town.Controller
+{
+    public class ValuesController : MonoBehaviour
+    {
+
+        [Header("Values Texts")]
+        [SerializeField] Text moneyText;
+        [SerializeField] Text citizensText;
+        [SerializeField] Text housesText;
+        [SerializeField] Text farmsText;
+        [SerializeField] Text orchardsText;
+
+        public void changeAllTexts(float currentMoney, int currentCitizens)
+        {
+            changeMoneyText(currentMoney);
+            changeCitizensText(currentCitizens);
+        }
+
+        public void changeMoneyText(float currentMoney)
+        {
+            moneyText.text = currentMoney.ToString();
+        }
+
+        public void changeCitizensText(int currentCitizens)
+        {
+            citizensText.text = currentCitizens.ToString();
+        }
+
+    }
+}
---
+++ _Core/GameController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Town.Controller
+{
+    public class GameController : MonoBehaviour
+    {
+
+        [Header("Initial Variables")]
+        [SerializeField] float money = 200f;
+        [SerializeField] int citizens = 5;
+        [SerializeField] int houses = 0;
+        [SerializeField] int farms = 0;
+        [SerializeField] int orchards = 0;
+        [SerializeField] float timeBetweenMoneyGive = 2f;
+
+        ValuesController valuesController;
+
+        private float currentMoney = 0;
+        private int currentCitizens = 0;
+        private int currentHouses = 0;
+        private int currentFarms = 0;
+        private int currentOrchards = 0;
+
+        public float GetCurrentMoney()
+        {
+            return this.currentMoney;
+        }
+
+        public void SetCurrentMoney(float quantity)
+        {
+            this.currentMoney += quantity;
+            valuesController.changeMoneyText(currentMoney);
+        }
+
+        public float GetCurrentCitizens()
+        {
+            return this.currentCitizens;
+        }
+
+        public void SetCurrentCitizens(int quantity)
+        {
+            this.currentCitizens += quantity;
+            valuesController.changeCitizensText(currentCitizens);
+        }
+
+        public float GetCurrentHouses()
+        {
+            return this.currentHouses;
+        }
+
+        public void SetCurrentHouses(int quantity)
+        {
+            this.currentHouses += quantity;
+        }
+
+        public float GetCurrentFarms()
+        {
+            return this.currentFarms;
+        }
+
+        public void SetCurrentFarms(int quantity)
+        {
+            this.currentFarms += quantity;
+        }
+
+        public float GetCurrentOrchards()
+        {
+            return this.currentOrchards;
+        }
+
+        public void SetCurrentOrchards(int quantity)
+        {
+            this.currentOrchards += quantity;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            valuesController = GameObject.FindGameObjectWithTag("Values Controller").GetComponent<ValuesController>();
+            ResetCurrentValues();
+        }
+
+        public void ResetCurrentValues()
+        {
+            this.currentMoney = this.money;
+            this.currentCitizens = this.citizens;
+            this.currentHouses = this.houses;
+            this.currentFarms = this.farms;
+            this.currentOrchards = this.orchards;
+            valuesController.changeAllTexts(currentMoney, currentCitizens);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            StartCoroutine(GiveMoney());
+        }
+
+        IEnumerator GiveMoney()
+        {
+            currentMoney += currentCitizens;
+            Debug.Log("Você recebeu " + currentCitizens + "$!");
+            valuesController.changeMoneyText(currentMoney);
+            yield return new WaitForSecondsRealtime(timeBetweenMoneyGive);
+        }
+    }
+}
